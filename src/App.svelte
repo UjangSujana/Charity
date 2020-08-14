ed084f407f358f1d6feefdde55e488a411eb1fcd
Ujang sujana <zoelfasujana@gmail.com>
2020-08-14 17:21:59 +0700

    Convert HTML to svelte components

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,5 @@
-<h1>Charity</h1>
+<script>
+	import Home from './Pages/Home.svelte';
+</script>
+
+<Home/>
---
+++ src/Pages/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+	import CharityList from '../Components/CharityList.svelte' ;
+	import Header from '../Components/Header.svelte';
+	import Wellcome from '../Components/Wellcome.svelte';
+	import Promo from '../Components/Promo.svelte';
+	import Footer from '../Components/Footer.svelte';
+
+    import { charities } from '../Data/charities.js';
+
+	let title = "Charity" ;
+
+	
+</script>
+
+
+<Header/>
+<Wellcome/>
+<CharityList {charities} />
+<Promo/>
+<Footer/>
